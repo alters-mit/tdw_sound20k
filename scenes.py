d3d9feb4f4fb712f43b7f54f81b9e021dc90475c
alters <alters@mit.edu>
2020-04-23 15:44:35 -0400

    cleanup

--- scenes.py
+++ scenes.py
@@ -1,14 +1,11 @@
 from tdw.controller import Controller
 from tdw.tdw_utils import TDWUtils
-from tdw.librarian import MaterialLibrarian, MaterialRecord, ModelLibrarian
+from tdw.librarian import ModelLibrarian
 from tdw.py_impact import PyImpact, AudioMaterial
-import numpy as np
+from audio_system import StandardAudio, ResonanceAudio, AudioSystem
 from typing import List, Dict, Tuple
 from abc import ABC, abstractmethod
-from audio_system import *
 from pathlib import Path
-
-RNG = np.random.RandomState(0)
 
 
 class Scene(ABC):
@@ -141,42 +138,21 @@ class Scene(ABC):
         raise Exception()
 
 
-class _ProcGenRoom(Scene):
+class _ProcGenRoom(Scene, ABC):
     """
     Initialize the ProcGen room.
     """
 
     def initialize_scene(self, c: Controller) -> List[dict]:
         commands = super().initialize_scene(c)
-        # Initialize the material library.
-        if c.material_librarian is None:
-            c.material_librarian = MaterialLibrarian()
-        material: MaterialRecord = self._get_floor_material(c.material_librarian)
         # Load the scene and an empty room.
         # Add the material to the floor.
         commands.extend([{"$type": "load_scene"},
                          TDWUtils.create_empty_room(12, 12),
-                         {"$type": "add_material",
-                          "name": material.name,
-                          "url": material.get_url()},
-                         {"$type": "set_proc_gen_floor_material",
-                          "name": material.name},
-                         {"$type": "set_proc_gen_floor_texture_scale",
-                          "scale": {"x": 8, "y": 8}},
                          {"$type": "set_proc_gen_walls_scale",
                           "walls": TDWUtils.get_box(12, 12),
                           "scale": {"x": 1, "y": 4, "z": 1}}])
         return commands
-
-    @abstractmethod
-    def _get_floor_material(self, lib: MaterialLibrarian) -> MaterialRecord:
-        """
-        :param lib: The material library.
-
-        :return: A visual material to apply to the floor.
-        """
-
-        raise Exception()
 
     def get_max_y(self) -> float:
         return 3.5
@@ -189,10 +165,6 @@ class FloorSound20k(_ProcGenRoom):
     """
     Initialize a scene with a floor that mimics a Sound20k scene (the floor is always wood).
     """
-
-    def _get_floor_material(self, lib: MaterialLibrarian) -> MaterialRecord:
-        wood_materials = lib.get_all_materials_of_type("Wood")
-        return wood_materials[RNG.randint(0, len(wood_materials))]
 
     def get_center(self, c: Controller) -> Dict[str, float]:
         return {"x": 0, "y": 0, "z": 0}
@@ -209,25 +181,6 @@ class CornerSound20k(_ProcGenRoom):
     Initialize a scene with a floor that mimics a Sound20k scene (the floor is always wood).
     The "center" is offset to a corner.
     """
-
-    def initialize_scene(self, c: Controller) -> List[dict]:
-        commands = super().initialize_scene(c)
-        # Set the wall material too.
-        mat_name = ""
-        for cmd in commands:
-            if cmd["$type"] == "set_proc_gen_floor_material":
-                mat_name = cmd["name"]
-                break
-        assert mat_name != ""
-        commands.extend([{"$type": "set_proc_gen_walls_material",
-                          "name": mat_name},
-                         {"$type": "set_proc_gen_walls_texture_scale",
-                          "scale": {"x": 8, "y": 8}}])
-        return commands
-
-    def _get_floor_material(self, lib: MaterialLibrarian) -> MaterialRecord:
-        wood_materials = lib.get_all_materials_of_type("Wood")
-        return wood_materials[RNG.randint(0, len(wood_materials))]
 
     def get_center(self, c: Controller) -> Dict[str, float]:
         return {"x": 4, "y": 0, "z": 4}
@@ -454,7 +407,7 @@ class DiningTableAndChairs(FloorSound20k):
         return commands
 
     def get_output_directory(self) -> str:
-        return super().get_output_directory() + "table-chairs"
+        return super().get_output_directory() + "_table-chairs"
 
 
 class DeskAndChair(FloorSound20k):
@@ -511,7 +464,7 @@ class DeskAndChair(FloorSound20k):
         return commands
 
     def get_output_directory(self) -> str:
-        return super().get_output_directory() + "desk-shelf-chair"
+        return super().get_output_directory() + "_desk-shelf-chair"
 
 
 SOUND20K = [FloorSound20k, CornerSound20k, LargeBowl, Ramp, StairRamp, UnevenTerrain, DiningTableAndChairs, DeskAndChair]
